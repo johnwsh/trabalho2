25775971f780bd2251164bb4117c8a5b3718562b
johnwsh <johnwillian0609@gmail.com>
2026-05-17 20:41:37 -0300

    mse function

--- src/trees.cpp
+++ src/trees.cpp
@@ -1,4 +1,6 @@
 #include "../include/trees.h"
+#include <algorithm>
+#include <limits.h>
 
 void buildBootstrappedDataset(vector<vector<double>>& X, 
                                                 vector<double>& Y, 
@@ -17,8 +19,47 @@ void buildBootstrappedDataset(vector<vector<double>>& X,
 
 }
 
-double calculateMSE(int attIndex, vector<vector<double>> &X, vector<double> &Y, double &threshold){
-    
+double calculateMSE(int attIndex, vector<vector<double>> &X, vector<double> &Y, double &threshold, mt19937 &generator){
+
+    vector<pair<double, double>> attAndY(X.size());
+    double sumTotal = 0;
+    for (int i = 0; i < X.size(); i++) {
+        attAndY[i].first = X[i][attIndex];
+        attAndY[i].second = Y[i];
+        //otimização espertinha pra não precisar recalcular
+        sumTotal += Y[i];
+    }
+    sort(attAndY.begin(), attAndY.end());
+
+    double error = INT_MAX;
+    double sumLeft = 0;
+    double sumRight = sumTotal;
+
+    for (int i = 0; i < attAndY.size() -1; i++){
+        double splitCandidate = (attAndY[i].first + attAndY[i+1].first)/2;
+        
+        sumLeft += attAndY[i].first;
+        sumRight -= attAndY[i].first;
+
+        double LeftMean = sumLeft/(i+1);
+        double RightMean = sumRight/(attAndY.size() - (i + 1));
+
+        double localError = 0;
+        for (int j = 0; j < attAndY.size(); j++){
+            if (attAndY[j].first < splitCandidate){
+                localError += (attAndY[j].second - LeftMean)*(attAndY[j].second - LeftMean);
+            }
+            else {
+                localError += (attAndY[j].second - RightMean)*(attAndY[j].second - RightMean);
+            }
+        }
+
+        if (localError < error){
+            error = localError;
+            threshold = splitCandidate;
+        }
+    }
+    return error;
 }
 
 Node* CART(int maxDepth, 
